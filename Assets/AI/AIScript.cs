c97867662e83c1d851ee2c6ae69c887109f1e35c
Dagon Zamora <5050uuk10a@gmail.com>
2024-10-01 22:43:55 -0500

    adjustments

--- Assets/AI/AIScript.cs
+++ Assets/AI/AIScript.cs
@@ -8,6 +8,10 @@ public class AIScript : MonoBehaviour
 {
     NavMeshAgent agent;
     [SerializeField] List<GameObject> points;
+    [SerializeField] GameObject player;
+    [SerializeField] Transform target;
+    States states = States.Patrol;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +23,34 @@ public class AIScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        void OnTriggerEnter(Collider player)
+        {
+
+        }
+
+        //dot product
+        Vector3 directionToTarget = (target.position - transform.position).normalized;
+        Vector3 forwardDirection = transform.forward;
+        float dot = Vector3.Dot(forwardDirection, directionToTarget);
+
+        if (dot > 0.5f ) { states = States.Pursue; }
+
+        switch (states)
+        {
+            case States.Patrol:
+                StartCoroutine(Patrol());
+                break;
+            case States.Investigate:
+                StopCoroutine(Patrol());
+                break;
+            case States.Pursue:
+                StopCoroutine(Patrol());
+                transform.position = target.position;
+                break;
+            default:
+                states = States.Patrol;
+                break;
+        }
     }
 
     IEnumerator Patrol()
@@ -35,5 +66,12 @@ public class AIScript : MonoBehaviour
                 i = 0;
             }
         }
+    }
+
+    enum States
+    {
+        Patrol,
+        Investigate,
+        Pursue
     }
 }
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] int speed;
+    [SerializeField] GameObject enemyVisionArea;
+    [SerializeField] GameObject enemyHearingArea;
     Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         StaticPlayerInput.myInput.Player0.Enable();
+    }
+
+    private void Update()
+    {
+        //if (StaticPlayerInput.myInput.Player0.Sneak.started)
+        //{
+        //    enemyVisionArea.transform.localScale /= 2;
+        //    enemyHearingArea.SetActive(false);
+        //}
+        //else if (StaticPlayerInput.myInput.Player0.Sneak.canceled)
+        //{
+        //    enemyVisionArea.transform.localScale *= 2;
+        //    enemyHearingArea.SetActive(true);
+        //}
     }
 
     private void FixedUpdate()
